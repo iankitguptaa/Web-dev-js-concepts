936b52ede6706b73c91693db05f14ebee66b2b37
Ankit Gupta <iankitgupta04@gmail.com>
2026-01-27 22:34:38 +0530

    Second commit

---
+++ JAVASCRIPT VIDEO 40(DATA TYPES & VARIABLES)/script.js
@@ -0,0 +1,40 @@
+// var age = 25;
+// if(true){
+//     console.log(age);
+// }
+
+// function solve() {
+//     var age = 25;
+//     console.log(age);
+// }
+
+// let :  It is a blocked scoped not globally scoped
+// {
+// let a = 10;
+// }
+// console.log(a);
+
+//  This is correct syntax
+// {
+//  let a = 10;
+//  console.log(a);
+// }
+
+// let a = 10;
+// a = "Ankit";
+// a = true;
+// console.log(a);
+
+// const = jo value ek baar assign ho gyi whi rahegi 
+// const a = 28;
+// console.log(a);
+
+// DataTypes 
+
+let marks = 20;
+marks = "Ankit";
+// marks = true;
+console.log(marks);
+
+console.log(typeof(marks))
+
