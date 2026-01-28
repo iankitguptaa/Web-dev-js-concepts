820bdf63a28f14b94d554530542217e484a81306
Ankit Gupta <iankitgupta04@gmail.com>
2026-01-28 20:46:27 +0530

    third commit

---
+++ VIDEO 41(Operators&conditionals)/scripts.js
@@ -0,0 +1,101 @@
+// Arithematic Operators
+
+// let a = 10;
+// let b = 5;
+// console.log(++a)
+
+// Assignment Opeators
+
+// let a = 5;
+// a = a + 10;
+// console.log(a);
+
+// Comparison Operators 
+
+// let a = 10;
+// let b = 5;
+// console.log(10>5);
+// console.log(10<5);
+
+// Ternary Operators
+// let age = 12;
+// let status1 = (age > 18) ? 'I can vote' : 'I cannot vote';
+// console.log(status1);
+
+// Logical Operators
+// AND - && - sab true hai - true
+//            agar koi ek bhi false hai - false
+// OR - || - agar koi ek bhi true hai - truue
+            //  agar salse hai toh false dega
+// NOT - !
+
+
+// let answer  = (true && true && true)
+// let answer  = (true && false && true)
+// console.log(answer);
+
+// let answer  = (true || true || true)
+// let answer  = (true || false || true)
+// console.log(answer);
+
+// let ans = !(false)
+// let ans = !(true);
+// console.log(ans);
+
+// Working with non-boolean
+// console.log(false || 'Ankit')
+// console.log(false || 7);
+
+// Bitwise Operators
+// AND - &
+// OR - |
+// NOT - ~
+// <<
+// >>
+// XOR ^
+
+// cONDITONALS 
+// IF-ELSE
+// SWITCH
+
+// let age = 21;
+// if(age >= 18){
+//     console.log("He can Vote")
+// }else{
+//     console.log("He can't vote")
+// }
+
+// let number = 5;
+// if(number == 1){
+//     console.log('A');
+// }
+// else if(number == 2){
+//     console.log('B');
+// }
+// else if(number == 3){
+//     console.log('C');
+// }
+// else if(number == 4){
+//     console.log('D');
+// }
+// else{
+//     console.log('E');
+// }
+
+// SWITCH CASE
+
+let number = 6;
+switch(number){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    case 4: console.log('D');
+    break;
+    default: console.log('E')
+}
+
+
+
