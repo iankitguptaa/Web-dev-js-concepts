fdff299f7f096d06c882182a623e9b07447e0d69
Ankit Gupta <iankitgupta04@gmail.com>
2026-01-29 22:23:21 +0530

    fix

--- VIDEO 42(LOOPS & STRINGS)/Script.js
+++ VIDEO 42(LOOPS & STRINGS)/Script.js
@@ -13,11 +13,19 @@
 //     }
 // }
 
-for(let i = 1; i <= 4; i++){
-    if(i == 3){
-        continue;
-    }
-        else{
-            console.log(i);
-        }
+// for(let i = 1; i <= 4; i++){
+//     if(i == 3){
+//         continue;
+//     }
+//         else{
+//             console.log(i);
+//         }
+// }
+
+
+// While loop
+let i = 1;
+while (i <= 5) {
+   console.log(i);
+   i++;
 }
