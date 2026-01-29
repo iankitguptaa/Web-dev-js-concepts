44e10abf0b6c7552326c47489af1cbecc486330d
Ankit Gupta <iankitgupta04@gmail.com>
2026-01-29 22:16:48 +0530

    fix bug

--- VIDEO 42(LOOPS & STRINGS)/Script.js
+++ VIDEO 42(LOOPS & STRINGS)/Script.js
@@ -1,5 +1,14 @@
-let a = "Ankit";
-for (let i = 0; i < 5; i++) {
-    console.log(a);
+// let a = "Ankit";
+// for (let i = 0; i < 5; i++) {
+//     console.log(a);
     
+// }
+
+for(let i = 1; i <= 6; i++){
+    if(i == 4){
+        break;
+    }
+    else{
+        console.log(i);
+    }
 }
