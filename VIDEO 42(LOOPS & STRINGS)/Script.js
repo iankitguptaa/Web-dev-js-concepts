7bf5cd07bfd6b91b3845742664e1062b2c2fe10e
Ankit Gupta <iankitgupta04@gmail.com>
2026-01-29 23:03:39 +0530

    fix

--- VIDEO 42(LOOPS & STRINGS)/Script.js
+++ VIDEO 42(LOOPS & STRINGS)/Script.js
@@ -24,8 +24,36 @@
 
 
 // While loop
-let i = 1;
-while (i <= 5) {
-   console.log(i);
-   i++;
-}
+// let i = 1;
+// while (i <= 5) {
+//    console.log(i);
+//    i++;
+// }
+
+// Strings
+
+// let firstname = "Ankit";
+// let lastname = "Gupta";
+// console.log(typeof(firstname))
+
+// let name = `Ankit 
+// Kumar`;
+// console.log(name);
+
+// let firstname = new String("Ankit Gupta")
+// console.log(firstname);
+
+// let op1 = 'English';
+// let op2 = 'Hindi';
+
+// console.log(op1.length);
+// console.log(op1.toUpperCase());
+// console.log(op1.toLowerCase());
+// let ans = op1 + op2;
+// console.log(ans);
+
+// let finalAns = `${op1}&${op2}`;
+// console.log(finalAns);
+
+let str = "Ankit";
+console.log(str.substring(2));
