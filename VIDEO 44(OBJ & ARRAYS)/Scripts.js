0fc6ba0bcd105323efda5998a98c7cf518fa29bf
Ankit Gupta <iankitgupta04@gmail.com>
2026-02-01 13:47:02 +0530

    fix

--- VIDEO 44(OBJ & ARRAYS)/Scripts.js
+++ VIDEO 44(OBJ & ARRAYS)/Scripts.js
@@ -2,6 +2,10 @@ let obj = {
     "name" : "Ankit",
     "age" : 25,
     "Weight" : 85,
-    "Height" : "6ft"
+    "Height" : "6ft",
+    greet : function(){
+        console.log("Hello ji kaise ho sare");
+    }
 }
 console.log(obj);
+obj.greet();
