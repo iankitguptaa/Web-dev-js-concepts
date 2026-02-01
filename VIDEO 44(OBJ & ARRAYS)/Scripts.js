b6ed7ea2eda4add4e4bb41bbc0ddd497d2bb55cf
Ankit Gupta <iankitgupta04@gmail.com>
2026-02-01 14:09:45 +0530

    fix

--- VIDEO 44(OBJ & ARRAYS)/Scripts.js
+++ VIDEO 44(OBJ & ARRAYS)/Scripts.js
@@ -1,11 +1,24 @@
-let obj = {
-    "name" : "Ankit",
-    "age" : 25,
-    "Weight" : 85,
-    "Height" : "6ft",
-    greet : function(){
-        console.log("Hello ji kaise ho sare");
-    }
-}
-console.log(obj);
-obj.greet();
+// let obj = {
+//     "name" : "Ankit",
+//     "age" : 25,
+//     "Weight" : 85,
+//     "Height" : "6ft",
+//     greet : function(){
+//         console.log("Hello ji kaise ho sare");
+//     }
+// }
+// console.log(obj);
+// obj.greet();
+
+// console.log(typeof(obj))
+
+// let obj2 = obj;
+
+
+// Creation of Arrays
+let arr = [1,2,3,4,5];
+console.log(arr);
+let brr = new Array('Ankit',1,true);
+console.log(brr);
+console.log(typeof(arr));
+console.log(typeof(brr));
